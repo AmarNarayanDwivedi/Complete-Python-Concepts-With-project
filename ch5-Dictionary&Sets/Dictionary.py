ca2da2dafb949a62ad7e203c93b1f6de9fd791eb
Amar Narayan Dwivedi <amardwivedi792@gmail.com>
2024-08-08 09:37:58 +0530

    Dictionary

---
+++ ch5-Dictionary&Sets/Dictionary.py
@@ -0,0 +1,27 @@
+# syntax 
+
+a = {
+    "key" : "value",
+    "key2" : "Amar",
+    "Amar" : "Narayan",
+    "list" : [1,2,3,34]
+}
+
+# accessing value by key
+print(a["key"])
+# accessing value by key
+print(a["list"])
+# accessing whole dictionary
+print(a)
+# print all keys
+print(a.keys())
+
+
+''' 
+PROPERTIES OF PYTHON DICTIONARIES 
+1. It is unordered. 
+2. It is mutable. 
+3. It is indexed. 
+4. Cannot contain duplicate keys.
+'''
+
---
+++ ch5-Dictionary&Sets/DictionaryMethode.py
@@ -0,0 +1,16 @@
+a = {
+    "key" : "value",
+    "key2" : "Amar",
+    "Amar" : "Narayan",
+    "list" : [1,2,3,34]
+}
+
+# Returns a list of (key,value)tuples. 
+print(a.items())
+
+# Returns a list containing dictionary's keys
+print(a.keys())
+
+
+
+
