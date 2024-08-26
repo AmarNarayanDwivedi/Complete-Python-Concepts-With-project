583b0d78d73a57aea9bcddc7554733486ad7477a
Amar Narayan Dwivedi <amardwivedi792@gmail.com>
2024-08-26 19:44:53 +0530

    Function and Recursion

--- ch8-Functions&Recursion/PracticeSet/P7.py
+++ ch8-Functions&Recursion/PracticeSet/P7.py
@@ -1,1 +1,11 @@
 # Write a python function to remove a given word from a list ad strip it at the same time. 
+
+def srtriper(l , word):
+    n = []
+    for item in l :
+        if not (item == word):
+            n.append(item.strip(word)) 
+    print(n)
+
+l = ["heel", " us", "use", "user"]
+srtriper(l,"us")
